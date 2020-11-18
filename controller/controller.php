a8780c9ad57235a55a8e56fd7b02a4b21af8428f
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-11-18 18:41:47 +0100

    Création du contrôleur

---
+++ controller/controller.php
@@ -0,0 +1,22 @@
+<?php
+require_once('model/PostsManager.php');
+require_once('model/CommentManager.php');
+
+//Appelle getPosts pour avoir et afficher la liste des billets
+function listPosts(){
+    $postsManager = new PostsManager();
+    $posts = $postsManager->getPosts();
+
+    require('view/listPostsView.php');
+}
+
+//Appelle getPost() pour avoir et afficher le billet sélectionner et getComment pour les commentaires
+function post(){
+    $postsManager = new PostsManager();
+    $commentManager = new CommentManager();
+
+    $post = $postsManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/postView.php');
+}
