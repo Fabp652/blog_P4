700c998a4266f7dc7a8904186cb13204480e6d84
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-11-23 14:21:32 +0100

    Correction de la connection PDO

--- model/Manager.php
+++ model/Manager.php
@@ -1,10 +1,12 @@
 <?php
 namespace P4\Blog\Model;
+use PDO;
 
 class Manager{
     protected function dbConnect(){
         try{
             $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $db;
         }
 
