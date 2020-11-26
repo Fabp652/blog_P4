4fb7587ace29ca99831d8fcf03cf7f46b0a43467
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-11-26 14:38:32 +0100

    Suppression des secondes de "creation_date"

--- model/PostsManager.php
+++ model/PostsManager.php
@@ -4,14 +4,14 @@ class PostsManager extends Manager{
     //Récupère les billets et les affiches
     public function getPosts(){
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
         return $req;
     }
 
     //Récupère un billet et l'affiche
     public function getPost($postId){
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = ?');
+        $req = $db->prepare('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin\') AS creation_date_fr FROM posts WHERE id = ?');
         $req->execute(array($postId));
         $post = $req->fetch();
         return $post;
