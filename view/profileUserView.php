5229f4a8c3952c23db44b0ed75364deb92e03d9b
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-12-04 16:54:50 +0100

    Ajout d'un lien pour modifier le mot de passe

--- view/profileUserView.php
+++ view/profileUserView.php
@@ -4,6 +4,7 @@
 <h1>Profil</h1>
 <p>Votre pseudo : <?php echo $_SESSION['pseudo'] ?></p>
 <p>Votre adresse email : <?php echo $_SESSION['email'] ?></p>
+<p><a href='index.php?action=change-password'>Modifier votre mot de passe</a></p>
 <?php 
     $content = ob_get_clean();
     require('view/template.php');
