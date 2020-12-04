3bc66351b67c15bbaba5da5a589a749057e60ce4
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-12-04 15:53:16 +0100

    Ajout d'un lien "déconnexion"

--- view/template.php
+++ view/template.php
@@ -13,12 +13,25 @@
         <li>
             <a href='index.php?action=listPost' class='nav-link'>Acceuil</a>
         </li>
+        <?php
+        //if($_SESSION['id'] != 'null' && $_SESSION['pseudo'] != 'null'){
+        ?>
+        <li>
+            <a href='index.php?action=logout' class='nav-link'>Déconnexion</a>
+        </li>
+        <?php
+        //}else{
+            //var_dump($_SESSION['id'], $_SESSION['pseudo']);die();
+        ?>
         <li>
             <a href='index.php?action=connection' class='nav-link'>Connexion</a>
         </li>
         <li>
             <a href='index.php?action=inscription' class='nav-link'>Inscription</a>
         </li>
+        <?php
+        //}
+        ?>
     </ul>
 </nav>
     <?php echo $content; ?>
