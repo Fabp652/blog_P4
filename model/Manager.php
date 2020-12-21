66697b468104a0b84146cbe70767387d29ec6a1f
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-12-21 14:21:33 +0100

    Modification du mot de passe de la base

--- model/Manager.php
+++ model/Manager.php
@@ -2,7 +2,7 @@
 class Manager{
     protected function dbConnect(){
         try{
-            $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+            $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $db;
         }
