48e823617abe1480b98fa8e583c37bb76f8ea77f
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-12-21 14:24:46 +0100

    Ajout de la condition isset($_SESSION['pseudo'])

--- view/postView.php
+++ view/postView.php
@@ -33,7 +33,7 @@ while($comment = $comments->fetch()){
 <p><?php echo htmlspecialchars($comment['author']); ?> le <?php echo $comment['comment_date_fr']; ?> :</p>
 <P><?php echo nl2br(htmlspecialchars($comment['comment'])); ?></P>
 <?php
-if($_SESSION['pseudo'] == $comment['author']){
+if(isset($_SESSION['pseudo']) && $_SESSION['pseudo'] == $comment['author']){
 ?>
 <a href="index.php?action=change-comment&amp;post-id=<?=$post['id']?>&amp;comment-id=<?=$comment['id']?>">Modifier</a>
 <a href="index.php?action=clear-comment&amp;post-id=<?=$post['id']?>&amp;comment-id=<?=$comment['id']?>">Supprimer</a>
