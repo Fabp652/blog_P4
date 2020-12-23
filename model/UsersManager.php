ee30eac24099ae049d11f79c18ff018016a860e8
Fabrice Pargny <fabrice.pargny@gmail.com>
2020-12-23 15:58:32 +0100

    Ajout de l'entrée role dans insertUser

--- model/UsersManager.php
+++ model/UsersManager.php
@@ -3,11 +3,12 @@ require_once('model/Manager.php');
 class UsersManager extends Manager{
     public function insertUser($pseudo, $password, $email){
         $db = $this->dbConnect();
-        $inscription = $db->prepare('INSERT INTO users(pseudo, pass, email, inscription_date) VALUES(:pseudo, :pass, :email, CURDATE())');
+        $inscription = $db->prepare('INSERT INTO users(pseudo, pass, email, role, inscription_date) VALUES(:pseudo, :pass, :email, :role, CURDATE())');
         $inscription->execute(array(
             'pseudo' => $pseudo,
             'pass' => $password,
-            'email' => $email
+            'email' => $email,
+            'role' => 'user'
         ));
         $inscription->closeCursor();
     }
