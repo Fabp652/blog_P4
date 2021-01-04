865c2ee90a2d63032b08502a95343c87d971708b
Fabrice Pargny <fabrice.pargny@gmail.com>
2021-01-04 16:02:35 +0100

    Ajout des commentaires signalés sur la page

--- view/adminView.php
+++ view/adminView.php
@@ -2,5 +2,15 @@
 
 <?php ob_start();?>
 <h1>Espace d'administration</h1>
+<h3>Commentaire(s) signalé(s)</h3>
+<?php
+while($report = $reportComments->fetch()){
+?>
+<p><?=getAuthor($report['user_id']) ?></p>
+<p><?=$report['comment'] ?></p>
+<a href="index.php?action=admin-clear-comment&amp;comment-id=<?=$report['id']?>">Supprimer</a>
+<?php
+}
+?>
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
