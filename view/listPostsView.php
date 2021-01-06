4e3e629e65f38debf1e704f707292c9698273ca7
Fabrice Pargny <fabrice.pargny@gmail.com>
2021-01-06 15:23:14 +0100

    stylisation des posts

--- view/listPostsView.php
+++ view/listPostsView.php
@@ -1,22 +1,22 @@
 <?php $title = 'Billet simple pour l\'Alaska'; ?>
 
 <?php ob_start(); ?>
-<h1>Billet simple pour l'Alaska</h1>
-<p>Bienvenue sur mon blog ! Vous trouverez ci-dessous les derniers chapitres publié : </p>
+<h1 class="text-center">Billet simple pour l'Alaska</h1>
+<p class="text-center">Bienvenue sur mon blog ! Vous trouverez ci-dessous les derniers chapitres publié : </p>
 
 <?php
 while ($blog = $posts->fetch()){
 ?>
 
-<div class='chapter'>
-    <h3>
+<div class='flex flex-col items-center border-2 border-solid border-blue-500'>
+    <h3 class="w-full text-center bg-blue-300 border-b-2 border-solid border-blue-500 py-2">
         <?=htmlspecialchars($blog['title']); ?>
     </h3>
     <p>
         <?=nl2br(htmlspecialchars($blog['content'])); ?>
     </p>
-    <a href="index.php?action=post&amp;id=<?= $blog['id'] ?>">Commentaires</a>
-    <p class='date_post'>Publié le <?= $blog['creation_date_fr']; ?></p>
+    <a href="index.php?action=post&amp;id=<?= $blog['id'] ?>" class="border border-solid border-blue-500 rounded-2xl p-1 mt-2 bg-blue-500">Commentaires</a>
+    <p class='self-end'>Publié le <?= $blog['creation_date_fr']; ?></p>
 </div>
 <?php 
     if(isset($_SESSION['is_admin']) && $_SESSION['is_admin'] == '1'){        
