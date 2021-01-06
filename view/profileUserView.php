6e6886d2ea7f479a2348cd0d3120bc078b55b376
Fabrice Pargny <fabrice.pargny@gmail.com>
2021-01-06 15:24:59 +0100

    Texte centré dans la page

--- view/profileUserView.php
+++ view/profileUserView.php
@@ -1,10 +1,10 @@
 <?php $title='profil' ?>
 
 <?php ob_start() ?>
-<h1>Profil</h1>
-<p>Votre pseudo : <?php echo $_SESSION['pseudo'] ?></p>
-<p>Votre adresse email : <?php echo $_SESSION['email'] ?></p>
-<p><a href='index.php?action=change-password'>Modifier votre mot de passe</a></p>
+<h1 class="text-center">Profil</h1>
+<p class="text-center">Votre pseudo : <?php echo $_SESSION['pseudo'] ?></p>
+<p class="text-center">Votre adresse email : <?php echo $_SESSION['email'] ?></p>
+<p class="text-center"><a href='index.php?action=change-password' class="border border-solid border-blue-500 rounded-2xl p-1 mt-2 bg-blue-500">Modifier votre mot de passe</a></p>
 <?php 
     $content = ob_get_clean();
     require('view/template.php');
