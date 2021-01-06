4d4737e68c3757cfd3b0ac21af522420618a9a66
Fabrice Pargny <fabrice.pargny@gmail.com>
2021-01-06 15:20:10 +0100

    Texte centré dans la page

--- view/adminView.php
+++ view/adminView.php
@@ -1,14 +1,14 @@
 <?php $title = 'Espace d\'administration'; ?>
 
 <?php ob_start();?>
-<h1>Espace d'administration</h1>
-<h3>Commentaire(s) signalé(s)</h3>
+<h1 class="text-center">Espace d'administration</h1>
+<h3 class="text-center">Commentaire(s) signalé(s)</h3>
 <?php
 while($report = $reportComments->fetch()){
 ?>
-<p><?=getAuthor($report['user_id']) ?></p>
-<p><?=$report['comment'] ?></p>
-<a href="index.php?action=admin-clear-comment&amp;comment-id=<?=$report['id']?>">Supprimer</a>
+<p class="text-center"><?=getAuthor($report['user_id']) ?></p>
+<p class="text-center"><?=$report['comment'] ?></p>
+<a href="index.php?action=admin-clear-comment&amp;comment-id=<?=$report['id']?>" class="self-center border border-solid border-blue-500 rounded-2xl p-1 bg-blue-500">Supprimer</a>
 <?php
 }
 ?>
