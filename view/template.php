c906a479ac704241a643367a6ab41ec48b0cf70e
Fabrice Pargny <fabrice.pargny@gmail.com>
2021-01-06 15:25:29 +0100

    Stylisation du menu de navigation

--- view/template.php
+++ view/template.php
@@ -3,43 +3,49 @@
 
 <head>
     <meta charset='utf-8' />
-    <link href="public/style.css" rel="stylesheet" />
+    <!-- <link href="public/style.css" rel="stylesheet" /> -->
+    <link href="public/tailwind.css" rel="stylesheet" /> 
+    <?php 
+    if(isset($script)){
+        echo $script;
+    }
+    ?>
     <title><?php echo $title; ?></title>
 </head>
 
 <body>
 <nav>
-    <ul class='nav-content'>
+    <ul class='flex justify-end'>
         <li>
-            <a href='index.php?action=listPost' class='nav-link'>Accueil</a>
+            <a href='index.php?action=listPost' class='p-2 hover:bg-blue-500'>Accueil</a>
         </li>
         <?php
         if(isset($_SESSION['pseudo'])){
             if($_SESSION['is_admin'] == '1'){
         ?>
         <li>
-            <a href="index.php?action=new-post&amp;user-id=<?=$_SESSION['id']?>" class='nav-link'>Créer un billet</a>
+            <a href="index.php?action=new-post&amp;user-id=<?=$_SESSION['id']?>" class='p-2 hover:bg-blue-500'>Créer un billet</a>
         </li>
         <li>
-            <a href="index.php?action=admin" class='nav-link'>Espace d'administration</a>
+            <a href="index.php?action=admin" class='p-2 hover:bg-blue-500'>Espace d'administration</a>
         </li>
         <?php
             }
         ?>
         <li>
-            <a href='index.php?action=profile' class='nav-link'>Profil</a>
+            <a href='index.php?action=profile' class='p-2 hover:bg-blue-500'>Profil</a>
         </li>
         <li>
-            <a href='index.php?action=logout' class='nav-link'>Déconnexion</a>
+            <a href='index.php?action=logout' class='p-2 hover:bg-blue-500'>Déconnexion</a>
         </li>
         <?php
         }else{
         ?>
         <li>
-            <a href='index.php?action=connection' class='nav-link'>Connexion</a>
+            <a href='index.php?action=connection' class='p-2 hover:bg-blue-500'>Connexion</a>
         </li>
         <li>
-            <a href='index.php?action=inscription' class='nav-link'>Inscription</a>
+            <a href='index.php?action=inscription' class='p-2 hover:bg-blue-500'>Inscription</a>
         </li>
         <?php
         }
