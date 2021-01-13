d3fd9c9a9d98bfee99dd07001a730f1a58023533
Fabrice Pargny <fabrice.pargny@gmail.com>
2021-01-13 11:19:02 +0100

    Ajout de class et lien vers style.css en head

--- view/template.php
+++ view/template.php
@@ -3,7 +3,9 @@
 
 <head>
     <meta charset='utf-8' />
-    <!-- <link href="public/style.css" rel="stylesheet" /> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script src="public/tailwind.config.js"></script>
+    <link href="public/style.css" rel="stylesheet" />
     <link href="public/tailwind.css" rel="stylesheet" /> 
     <?php 
     if(isset($script)){
@@ -14,45 +16,46 @@
 </head>
 
 <body>
-<nav>
-    <ul class='flex justify-end'>
-        <li>
-            <a href='index.php?action=listPost' class='p-2 hover:bg-blue-500'>Accueil</a>
-        </li>
-        <?php
-        if(isset($_SESSION['pseudo'])){
-            if($_SESSION['is_admin'] == '1'){
-        ?>
-        <li>
-            <a href="index.php?action=new-post&amp;user-id=<?=$_SESSION['id']?>" class='p-2 hover:bg-blue-500'>Créer un billet</a>
-        </li>
-        <li>
-            <a href="index.php?action=admin" class='p-2 hover:bg-blue-500'>Espace d'administration</a>
-        </li>
-        <?php
+    <nav class="pr-20 h-16 fixed w-full top-0">
+        <ul class='flex justify-end h-full items-center'>
+            <li>
+                <a href='index.php?action=listPost' class='font-medium p-4 bg-gradient-to-t hover:from-blue-600 hover:to-blue-300 rounded-sm'>Accueil</a>
+            </li>
+            <?php
+            if(isset($_SESSION['pseudo'])){
+                if($_SESSION['is_admin'] == '1'){
+            ?>
+            <li>
+                <a href="index.php?action=new-post&amp;user-id=<?=$_SESSION['id']?>" class='font-medium p-4 bg-gradient-to-t hover:from-blue-600 hover:to-blue-300 rounded-sm'>Créer un billet</a>
+            </li>
+            <li>
+                <a href="index.php?action=admin" class='font-medium p-4 bg-gradient-to-t hover:from-blue-600 hover:to-blue-300 rounded-sm'>Espace d'administration</a>
+            </li>
+            <?php
+                }
+            ?>
+            <li>
+                <a href='index.php?action=profile' class='font-medium p-4 bg-gradient-to-t hover:from-blue-600 hover:to-blue-300 rounded-sm'>Profil</a>
+            </li>
+            <li>
+                <a href='index.php?action=logout' class='font-medium p-4 bg-gradient-to-t hover:from-blue-600 hover:to-blue-300 rounded-sm'>Déconnexion</a>
+            </li>
+            <?php
+            }else{
+            ?>
+            <li>
+                <a href='index.php?action=connection' class='font-medium p-4 bg-gradient-to-t hover:from-blue-600 hover:to-blue-300 rounded-sm'>Connexion</a>
+            </li>
+            <li>
+                <a href='index.php?action=inscription' class='font-medium p-4 bg-gradient-to-t hover:from-blue-600 hover:to-blue-300 rounded-sm'>Inscription</a>
+            </li>
+            <?php
             }
-        ?>
-        <li>
-            <a href='index.php?action=profile' class='p-2 hover:bg-blue-500'>Profil</a>
-        </li>
-        <li>
-            <a href='index.php?action=logout' class='p-2 hover:bg-blue-500'>Déconnexion</a>
-        </li>
-        <?php
-        }else{
-        ?>
-        <li>
-            <a href='index.php?action=connection' class='p-2 hover:bg-blue-500'>Connexion</a>
-        </li>
-        <li>
-            <a href='index.php?action=inscription' class='p-2 hover:bg-blue-500'>Inscription</a>
-        </li>
-        <?php
-        }
-        ?>
-    </ul>
-</nav>
+            ?>
+        </ul>
+    </nav>
     <?php echo $content; ?>
+    <!-- <div class="absolute top-0 right-0 bottom-0 left-0 bg-black opacity-50 index h-full "></div> -->
 </body>
 
 </html>
