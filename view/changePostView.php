0ee5556fb0e3a42f81408e4af39e8979b50cbfec
Fabrice Pargny <fabrice.pargny@gmail.com>
2021-01-15 16:36:40 +0100

    Ajout de style avec tailwind pour le responsive

--- view/changePostView.php
+++ view/changePostView.php
@@ -10,14 +10,14 @@ tinymce.init({
 <?php $script = ob_get_clean();?>
 <?php $title = 'Modfication du billet';?>
 <?php ob_start();?>
-<h1 class="text-center text-white text-4xl pt-20 mb-2">Modification du billet</h1>
-<div class="bg-white w-4/5 m-auto p-10 rounded-xl">
+<h1 class="text-center text-4xl pt-20 mb-2">Modification du billet</h1>
+<div class="bg-white w-full py-6 md:w-4/5 m-auto md:p-6 rounded-xl">
 <form action="index.php?action=update-post&amp;id=<?=$_GET['id']?>" method="POST" class="flex flex-col items-center justify-between w-full">
     <label class="my-2">
         Titre : <input type="text" name="title" class="border border-solid focus:border-blue-800 focus:outline-none" />
     </label>
     <textarea name="content" class="my-2"></textarea>
-    <input type="submit" name="Modifier" class="font-medium border border-solid border-blue-300 rounded-3xl py-2 px-3 bg-blue-300 my-2 cursor-pointer hover:bg-blue-400" />
+    <input type="submit" name="update" value="Modifier le billet" class="font-medium border border-solid border-blue-300 rounded-3xl py-2 px-3 bg-blue-300 my-2 cursor-pointer hover:bg-blue-400 text-sm md:text-base" />
 </form>
 </div>
 <?php 
